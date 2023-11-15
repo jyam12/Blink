cd6d610c659eef5d4081f54643b38c6fbe2ffe2e
HE, Wenqian 何文謙 <94374732+timho1047@users.noreply.github.com>
2023-11-16 02:02:16 +0800

    added some html code to question asked

--- js/question_asked.js
+++ js/question_asked.js
@@ -1,3 +1,42 @@
 import { getQuestionHistory } from "./lib/history.js";
 
 const history = getQuestionHistory();
+
+// #region Report Popup
+
+const openReportBtn = document.getElementById("open-report-btn");
+const closeReportBtn = document.getElementById("close-report-btn");
+const reportPopupElem = document.getElementById("report-popup");
+
+/**
+ * Event handler for openFormBtn
+ */
+openReportBtn.addEventListener(
+  "click",
+  () => (reportPopupElem.style.display = "block")
+);
+
+/**
+ * Event handler for closeFormBtn
+ */
+closeReportBtn.addEventListener(
+  "click",
+  () => (reportPopupElem.style.display = "none")
+);
+
+// #endregion
+
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
