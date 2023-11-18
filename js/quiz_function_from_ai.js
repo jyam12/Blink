82839b98dc96bd1406819866bb1e9e4229de68db
HE, Wenqian 何文謙 <94374732+timho1047@users.noreply.github.com>
2023-11-18 14:19:38 +0800

    finish quiz functions

--- js/quiz_function_from_ai.js
+++ js/quiz_function_from_ai.js
@@ -1,65 +1,306 @@
-import { QuizDatabase } from "/js/lib/quiz.js";
+import { QuizDatabase, QuizEntry } from "/js/lib/quiz.js";
 import { GPTManger, PromptGenerator } from "/js/lib/gpt.js";
+import { HistoryDatabase, HistoryEntry } from "/js/lib/history.js";
+
+const defaultQuizEntry = new QuizEntry(
+  0,
+  "Please wait...",
+  "Please wait...",
+  "0:00"
+);
 
 class QuizEntryComponent {
-  constructor(quizEntry, videoElem, questionGpt, answerGpt) {
-    this.quizEntry = quizEntry;
-    this.video = videoElem;
-    this.questionGpt = questionGpt;
-    this.answerGpt = answerGpt;
+  constructor(entry, quizManager) {
+    this.quizManager = quizManager;
 
-    // this.elem = this.setup();
+    if (entry) this.entry = entry;
+    else this.entry = defaultQuizEntry;
+
+    this.setup();
+
+    if (!entry) {
+      console.log("defaultQuizEntry");
+      this.quizManager.generateEntry().then((newEntry) => {
+        this.update(newEntry);
+      });
+    }
   }
+
+  //#region setup
 
   setup() {
-    setupOperationsComponent();
-    // setupQuestion();
-    // setupEntry();
+    this.basicElems = this.constructBasicElems(this.entry.id);
+    this.operationElem = this.constructOperationElem(this.basicElems);
+    this.elem = this.constructElem(
+      this.basicElems,
+      this.operationElem,
+      this.entry.id
+    );
+    this.addEventListeners();
   }
 
-  setupOperationsComponent() {}
+  constructBasicElems(id) {
+    const queElem = document.createElement("textarea");
+    queElem.classList.add("quiz-question");
+    queElem.classList.add("collapsible");
+    queElem.value = this.entry.question;
+
+    const ansElem = document.createElement("textarea");
+    ansElem.classList.add("quiz-answer");
+    ansElem.value = this.entry.answer;
+
+    const labelElem = document.createElement("label");
+    labelElem.innerText = "Timestamp: ";
+    labelElem.setAttribute("for", "timestamp");
+
+    const timeStampElem = document.createElement("input");
+    timeStampElem.setAttribute("type", "text");
+    timeStampElem.setAttribute("name", "timestamp-" + id);
+    timeStampElem.setAttribute("placeholder", "0:00");
+    timeStampElem.classList.add("timestamp");
+    timeStampElem.value = this.entry.timeStamp;
+
+    const regenerateQnABtnElem = document.createElement("button");
+    regenerateQnABtnElem.innerText = "Regenerate Q&A";
+    regenerateQnABtnElem.classList.add("regenerate-qna");
+
+    const regenerateAnsBtnElem = document.createElement("button");
+    regenerateAnsBtnElem.innerText = "Regenerate Answer";
+    regenerateAnsBtnElem.classList.add("regenerate-ans");
+
+    const saveBtnElem = document.createElement("button");
+    saveBtnElem.innerText = "Save";
+    saveBtnElem.classList.add("save");
+
+    const deleteBtnElem = document.createElement("button");
+    deleteBtnElem.innerText = "Delete";
+    deleteBtnElem.classList.add("delete-quiz");
+
+    return {
+      queElem,
+      ansElem,
+      labelElem,
+      timeStampElem,
+      regenerateQnABtnElem,
+      regenerateAnsBtnElem,
+      saveBtnElem,
+      deleteBtnElem,
+    };
+  }
+
+  addEventListeners() {
+    this.basicElems.queElem.addEventListener("change", (e) => {
+      e.target.style.height = "1px";
+      e.target.style.height = e.target.scrollHeight + "px";
+    });
+    this.basicElems.queElem.addEventListener("click", (e) => {
+      const contentStyle = this.operationElem.style;
+      contentStyle.display = contentStyle.display === "none" ? "block" : "none";
+    });
+
+    this.basicElems.ansElem.addEventListener("focus", (e) => {
+      e.target.style.height = "1px";
+      e.target.style.height = 25 + e.target.scrollHeight + "px";
+    });
+    this.basicElems.ansElem.addEventListener(
+      "focusout",
+      (e) => (e.target.style.height = 100 + "%")
+    );
+
+    this.basicElems.regenerateQnABtnElem.addEventListener("click", () =>
+      this.regenerateQnA()
+    );
+
+    this.basicElems.regenerateAnsBtnElem.addEventListener("click", () =>
+      this.regenerateAns()
+    );
+
+    this.basicElems.saveBtnElem.addEventListener("click", () => this.save());
+
+    this.basicElems.deleteBtnElem.addEventListener("click", () =>
+      this.delelete()
+    );
+  }
+
+  constructOperationElem(basicElements) {
+    const operationElem = document.createElement("div");
+    operationElem.classList.add("operations");
+    operationElem.classList.add("content");
+    for (const key in basicElements)
+      if (key != "queElem") operationElem.appendChild(basicElements[key]);
+
+    operationElem.style.display = "none";
+
+    return operationElem;
+  }
+
+  constructElem(basicElements, operationElem, id) {
+    const elem = document.createElement("div");
+    elem.classList.add("quiz-entry");
+    elem.appendChild(basicElements.queElem);
+    elem.appendChild(operationElem);
+    elem.setAttribute("id", id);
+    return elem;
+  }
+
+  //#endregion
+
+  //#region operations
+
+  update(entry) {
+    this.entry = entry;
+    this.basicElems.ansElem.value = entry.answer;
+    this.basicElems.queElem.value = entry.question;
+    this.basicElems.timeStampElem.value = entry.timeStamp;
+    this.basicElems.labelElem.setAttribute("for", "timestamp-" + entry.id);
+    this.elem.setAttribute("id", entry.id);
+  }
+
+  delelete() {
+    this.quizManager.deleteEntry(this.entry);
+    this.elem.remove();
+  }
+
+  save() {
+    this.entry.question = this.basicElems.queElem.value;
+    this.entry.answer = this.basicElems.ansElem.value;
+    this.entry.timeStamp = this.basicElems.timeStampElem.value;
+    this.quizManager.updateEntry(this.entry);
+  }
+
+  async regenerateQnA() {
+    this.basicElems.queElem.value = "Please wait...";
+    this.basicElems.ansElem.value = "Please wait...";
+
+    this.save();
+    this.entry.question = await this.quizManager.generateQuestion();
+    this.entry.answer = await this.quizManager.generateAnswer(
+      this.entry.question
+    );
+
+    this.quizManager.updateEntry(this.entry);
+    this.update(this.entry);
+  }
+
+  async regenerateAns() {
+    this.basicElems.ansElem.value = "Please wait...";
+
+    this.save();
+    this.entry.answer = await this.quizManager.generateAnswer(
+      this.entry.question
+    );
+
+    this.quizManager.updateEntry(this.entry);
+    this.update(this.entry);
+  }
+
+  //#endregion
 }
 
-/**
- * Get thevideo pause time
- * @returns {String} the video pause time in string format
- */
-function getVideoPauseTime() {
-  const video = document.getElementById("video");
-  const time = video.currentTime;
+class QuizManager {
+  constructor(quizListElem) {
+    this.quizListElem = quizListElem;
+  }
 
-  const m = Math.floor(time / 60);
-  const s = Math.floor(time - m * 60);
+  //#region setup
+  setup(quizDatabase, queGpt, ansGpt) {
+    this.quizDatabase = quizDatabase;
+    this.queGpt = queGpt;
+    this.ansGpt = ansGpt;
+    this.setupEntryComponents();
+    this.newQuizBtnElem = this.setupNewQuizBtnElem();
+  }
 
-  const timeString = `${m}:${s}`;
-  return timeString;
-}
+  setupEntryComponents() {
+    this.quizListElem.innerHTML = "";
+    const entries = this.quizDatabase.quiz;
 
-async function generateAndDisplayQuizQuestion() {
-  // #todo
-}
+    entries.forEach((quizEntry) => {
+      const component = new QuizEntryComponent(quizEntry, this);
+      this.quizListElem.appendChild(component.elem);
+    });
+  }
 
-async function generateAndDisplayQuizAnswer() {
-  // #todo
-}
+  setupNewQuizBtnElem() {
+    const newQuizBtnElem = document.createElement("button");
+    newQuizBtnElem.innerText = "New Quiz";
+    newQuizBtnElem.classList.add("new_quiz");
 
-function displayQuizQuestion() {
-  // #todo
+    newQuizBtnElem.addEventListener("click", () => {
+      let num = window.prompt(
+        "How many new quiz entries do you want to generate?",
+        1
+      );
+      num = parseInt(num);
+      if (isNaN(num)) return;
+      this.addNewEntryComponents(num);
+    });
+
+    this.quizListElem.appendChild(newQuizBtnElem);
+    return newQuizBtnElem;
+  }
+
+  //#endregion
+
+  //#oerations
+
+  addNewEntryComponents(num) {
+    for (let i = 0; i < num; i++) {
+      const newEntryComponent = new QuizEntryComponent(null, this);
+      this.quizListElem.insertBefore(
+        newEntryComponent.elem,
+        this.newQuizBtnElem
+      );
+    }
+  }
+
+  deleteEntry(entry) {
+    this.quizDatabase.deleteById(entry.id);
+  }
+
+  updateEntry(entry) {
+    this.quizDatabase.updateQuestionById(entry.id, entry.question);
+    this.quizDatabase.updateAnswerById(entry.id, entry.answer);
+    this.quizDatabase.updateTimeStampById(entry.id, entry.timeStamp);
+  }
+
+  addEntry(entry) {
+    return this.quizDatabase.addEntry(entry.question, entry.answer);
+  }
+
+  async generateQuestion() {
+    return await this.queGpt.send(null);
+  }
+
+  async generateAnswer(question) {
+    return await this.ansGpt.send(question);
+  }
+
+  async generateEntry() {
+    const question = await this.generateQuestion();
+    const answer = await this.generateAnswer(question);
+    const entry = this.quizDatabase.addEntry(question, answer);
+    return entry;
+  }
 }
 
 // #endregion
 
-// quiz
-var coll = document.getElementsByClassName("collapsible");
+// #region main
+const quizDatabase = new QuizDatabase();
+const historyDatabase = new HistoryDatabase();
 
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+const pg = new PromptGenerator();
+pg.setup(historyDatabase);
+
+const queGpt = new GPTManger();
+queGpt.setup(pg.quizQuestion.bind(pg));
+
+const ansGpt = new GPTManger();
+ansGpt.setup(pg.quizAnswer.bind(pg));
+
+const quizListElem = document.getElementById("quiz-list");
+const quizManager = new QuizManager(quizListElem);
+quizManager.setup(quizDatabase, queGpt, ansGpt);
+
+// #endregion
