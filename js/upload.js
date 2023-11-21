f7cba39107df7961b3b27ae643180e13884b6457
HE, Wenqian 何文謙 <94374732+timho1047@users.noreply.github.com>
2023-11-22 01:28:50 +0800

    change the event of upload

--- js/upload.js
+++ js/upload.js
@@ -2,11 +2,12 @@ const lectureCodeInput = document.getElementById("lecture-code");
 const videoInput = document.getElementById("video");
 const lectureNoteInput = document.getElementById("lecture-note");
 const uploadBtn = document.getElementById("upload");
-const successMsg = document.getElementById("success");
 
 uploadBtn.addEventListener("click", () => {
   lectureCodeInput.value = null;
   videoInput.value = null;
   lectureNoteInput.value = null;
-  successMsg.classList.add("show");
+  window.alert(
+    "Upload successful! Now you can go to Student view to check the video!"
+  );
 });
